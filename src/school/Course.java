7f6c62666501a7852919383661d0d3f98890e31f
unknown <145003490@mvhsrop901-12.vusdnoc.vusd.k12.ca.us>
2015-09-14 11:07:24 -0700

    School

--- src/school/Course.java
+++ src/school/Course.java
@@ -8,4 +8,65 @@ public class Course {
     private Type type;
     private String name;
     private int period;
+    public static Course addCourse(String _name, Type _type, int _period)
+    {
+        Course temp = new Course (_name,_type,_period);
+        course.add(temp);
+        return(temp);
+    }
+    Course()
+    {
+        name = "None";
+        type = Type.Math;
+        period = 1;
+    }
+    Course(String _name, Type _type, int _period)
+    {
+        name = _name;
+        type = _type;
+        period = _period;
+    }
+     public void setName(String _name)
+    {
+        name = _name;
+    }
+    public void setPeriod(int _period)
+    {
+        period = _period;
+    }
+    public void setType(Type _type)
+    {
+        type = _type;
+    }
+    public Type getType()
+    {
+        return(type);
+    }
+    public int getPeriod()
+    {
+        return(period);
+    }
+    public String getName()
+    {
+        return(name);
+    }
+    public static void printClass(Course.Type _type)
+    {
+        System.out.println("==========");
+        for( Course temp : course)
+            {
+                if(temp.type == _type)
+                System.out.println(temp.getName() + " Period: "+ temp.period);
+            }
+
+    }
+    public static void printClasses()
+    {
+        System.out.println("==========");
+        for( Course temp : course)
+            {
+                System.out.println(temp.getName() + " Period: "+ temp.period);
+            }
+
+    }
 }
--- src/school/School.java
+++ src/school/School.java
@@ -6,10 +6,19 @@ package school;
 public class School {
     public static void main(String[] args) {
         
+        Course Math = Course.addCourse("Math", Course.Type.Math, 1);
+        Course English = Course.addCourse("English", Course.Type.English, 2);
+        Course Science = Course.addCourse("Science", Course.Type.Science, 3);
+        Course PE = Course.addCourse("PE", Course.Type.PE, 4);
+        Course Elective = Course.addCourse("Elective", Course.Type.Elective, 5);
         Person Joe = Person.addPerson("Joe",Person.Gender.Male,120);
         Person Jill = Person.addPerson("Jill",Person.Gender.Female,150);
         Person Fats = Person.addPerson("Fats",Person.Gender.Male,9000000);
         Person Uber = Person.addPerson("Ubermensch",Person.Gender.Male,145);
         Person.printName(Person.Gender.Male);
+        Course.printClasses();
+        Course.printClass(Course.Type.PE);
+        
+        
     }
 }
